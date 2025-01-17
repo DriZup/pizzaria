44105daaeb96d607e62ddd60cfbd520d04cf07c2
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-01-17 08:59:58 -0300

    feat: adicionar ClienteController para criar o cliente

---
+++ src/main/java/com/zup/pizzaria/controllers/ClienteController.java
@@ -0,0 +1,30 @@
+package com.zup.pizzaria.controllers;
+
+import com.zup.pizzaria.dtos.ClienteDTO;
+import com.zup.pizzaria.models.Cliente;
+import com.zup.pizzaria.services.ClienteService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> criarCliente(@Valid @RequestBody ClienteDTO clienteDTO) {
+        Cliente clienteCriado = clienteService.criarCliente(clienteDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteCriado);
+    }
+}
