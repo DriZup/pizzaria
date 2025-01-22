198fd4dcb098cf853761e39e4a7938ece59a7df6
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-01-22 13:20:52 -0300

    refactor: melhoramentos na classe ClienteService

--- src/main/java/com/zup/pizzaria/services/ClienteService.java
+++ src/main/java/com/zup/pizzaria/services/ClienteService.java
@@ -6,6 +6,7 @@ import com.zup.pizzaria.repository.ClienteRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -17,20 +18,31 @@ public class ClienteService {
     }
 
     public Cliente criarCliente(ClienteDTO clienteDTO) {
-        Cliente cliente = new Cliente(
-                clienteDTO.getNome(),
-                clienteDTO.getEmail()
-        );
-       return clienteRepository.save(cliente);
-    }
-
-    public Cliente buscarClientePorId(Long id) {
-        return clienteRepository
-                .findById(id)
-                .orElseThrow(() -> new RuntimeException("Cliente nao encontrado"));
+        Cliente cliente = new Cliente(clienteDTO.getNome(), clienteDTO.getEmail());
+        return clienteRepository.save(cliente);
     }
 
     public List<Cliente> listarClientes() {
         return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarClientePorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public Optional<Cliente> atualizarCliente(Long id, Cliente clienteAtualizado) {
+        return clienteRepository.findById(id).map(cliente -> {
+            cliente.setNome(clienteAtualizado.getNome());
+            cliente.setEmail(clienteAtualizado.getEmail());
+            return clienteRepository.save(cliente);
+        });
+    }
+
+    public boolean deletarCliente(Long id) {
+        if (clienteRepository.existsById(id)) {
+            clienteRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
