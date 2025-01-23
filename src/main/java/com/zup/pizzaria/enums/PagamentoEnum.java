8c03b1518ac6f1ab6618ca4ec6e6aec40c1774a2
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-01-23 09:16:45 -0300

    refactor: Adicionado suporte para valores customizados com @JsonProperty e mapeamento de 'Cartão de Crédito'

--- src/main/java/com/zup/pizzaria/enums/PagamentoEnum.java
+++ src/main/java/com/zup/pizzaria/enums/PagamentoEnum.java
@@ -1,8 +1,13 @@
 package com.zup.pizzaria.enums;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum PagamentoEnum {
-    CARTAO,
+    @JsonProperty("Dinheiro")
     DINHEIRO,
-    PIX;
+    @JsonProperty("Cartão de Crédito")
+    CARTAO,
+    @JsonProperty("Pix")
+    PIX
 }
